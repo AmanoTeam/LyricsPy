e2fde1a049ecc7a1b74b7d5507b52367c90ffffc
Alisson Lauffer <alissonvitortc@gmail.com>
2019-06-12 21:28:08 -0300

    Some improvments and changes to the code

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
+with open("README.md") as f:
+    long_desc = f.read()
+
 setuptools.setup(
     name='LyricsPy',
     version='0.1.1',
     packages=setuptools.find_packages(),
     url='https://github.com/amanoteam/LyricsPy',
     author='AmanoTeam',
-    install_requires=['requests','bs4','google','markdownify'],
+    install_requires=['requests', 'bs4', 'google', 'markdownify'],
     author_email='contact@amanoteam.ml',
     description='search lyrics on letras.mus',
-    long_description='No description for now',
+    long_description=long_desc,
     long_description_content_type="text/markdown"
 )
