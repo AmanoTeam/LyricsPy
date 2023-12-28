347efac791873151404a49a5fac4a1777ac91780
Alisson Lauffer <alissonvitortc@gmail.com>
2023-12-28 17:20:58 -0300

    migrate project to pyproject.toml

--- lyricspy/__init__.py
+++ lyricspy/__init__.py
@@ -5,7 +5,7 @@ from typing import Union
 import httpx
 from bs4 import BeautifulSoup
 
-from asyncio import get_event_loop
+__version__ = '3.0.0a0'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.37'}
